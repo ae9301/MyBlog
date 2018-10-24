1d369750f59717570319d1a0a0c4df3ba7344218
mokhan <mokhan@BLRL59984.iis.amadeus.net>
2018-10-24 08:39:22 +0530

    Change File header which has private company marketting name

--- SpringJTAOnWildFly/src/main/java/com/sarf/repository/EmployeeRepository.java
+++ SpringJTAOnWildFly/src/main/java/com/sarf/repository/EmployeeRepository.java
@@ -1,8 +1,3 @@
-/*
- * Amadeus Confidential Information:
- * Unauthorized use and disclosure strictly forbidden.
- * @1998-2012 - Amadeus s.a.s - All Rights Reserved.
- */
 package com.sarf.repository;
 
 import org.springframework.data.domain.Page;
